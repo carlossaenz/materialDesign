965a9cd69f2a88f8708cf05c675402a3f76151bd
Carlos Saenz <saenz.carlos@me.com>
2017-12-20 19:11:22 -0600

    Initial Commit

---
+++ Podfile
@@ -0,0 +1,22 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'material design' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+
+  # Pods for material design
+
+  target 'material designTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'material designUITests' do
+    inherit! :search_paths
+    # Pods for testing
+    pod 'MaterialComponents'
+ 
+
+  end
+
+end
---
+++ material design/DetailViewController.swift
@@ -0,0 +1,45 @@
+//
+//  DetailViewController.swift
+//  material design
+//
+//  Created by Carlos Sáenz on 12/20/17.
+//  Copyright © 2017 Carlos Sáenz. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = detailItem {
+            if let label = detailDescriptionLabel {
+                label.text = detail.description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        configureView()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    var detailItem: NSDate? {
+        didSet {
+            // Update the view.
+            configureView()
+        }
+    }
+
+
+}
+
